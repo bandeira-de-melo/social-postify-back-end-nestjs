82e0ffe2fdd1f36d112c1adb039398dac4f7c315
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-08-28 17:44:50 -0300

    feat: integration tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaModule } from '../src/prisma/prisma.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+  
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, PrismaModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    prisma = app.get(PrismaService);
+
+    await prisma.publication.deleteMany({});
+    await prisma.post.deleteMany({});
+    await prisma.media.deleteMany({})
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    await request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('POST /medias should return Status Code 400 if info is not sent in body', async () => {
+     await request(app.getHttpServer())
+    .post('/medias')
+    .send({})
+    .expect(HttpStatus.BAD_REQUEST);
+  })
+
+  it('POST /posts should return Status Code 400 if info is not sent in body', async () => {
+     await request(app.getHttpServer())
+    .post('/posts')
+    .send({})
+    .expect(HttpStatus.BAD_REQUEST);
+  })
 });
